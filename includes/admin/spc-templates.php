e94cae0125c9b68cbf0de89d028c3fbacb649479
Ashar Irfan <mrasharirfan@gmail.com>
2019-03-25 00:36:35 +0500

    👌 IMPROVE: Primary Term Selector Template Style

--- includes/admin/spc-templates.php
+++ includes/admin/spc-templates.php
@@ -10,17 +10,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
+<style>
+	.spc-primary-term select { width: 100% }
+</style>
+
 <script type="text/html" id="tmpl-spc-select-primary-term">
-	<p class="spc-primary-term-heading"><strong>Primary {{data.taxonomy.title}}</strong></p>
-	<select class="spc-primary-term" id="spc-primary-term-{{data.taxonomy.name}}" name="spc_primary_term_{{data.taxonomy.name}}">
-		<option value="-1">— Select Primary {{data.taxonomy.title}} —</option>
-		<# _( data.taxonomy.terms ).each( function( term ) { #>
-			<option value="{{term.id}}"
-			<# if ( data.taxonomy.primary === term.id ) { #>
-				selected
-			<# } #>
-			>{{term.name}}</option>
-		<# }); #>
-	</select>
-	<?php wp_nonce_field( 'spc-save-primary-term', 'spc_save_primary_{{data.taxonomy.name}}_nonce' ); ?>
+	<div class="spc-primary-term">
+		<p class="spc-primary-term-heading"><strong>Primary {{data.taxonomy.title}}</strong></p>
+		<select id="spc-primary-term-{{data.taxonomy.name}}" name="spc_primary_term_{{data.taxonomy.name}}">
+			<option value="-1">— Select Primary {{data.taxonomy.title}} —</option>
+			<# _( data.taxonomy.terms ).each( function( term ) { #>
+				<option value="{{term.id}}"
+				<# if ( data.taxonomy.primary === term.id ) { #>
+					selected
+				<# } #>
+				>{{term.name}}</option>
+			<# }); #>
+		</select>
+		<?php wp_nonce_field( 'spc-save-primary-term', 'spc_save_primary_{{data.taxonomy.name}}_nonce' ); ?>
+	</div>
 </script>
